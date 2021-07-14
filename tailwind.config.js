78405592e652e173a1976d3901d3243704d531d0
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2021-07-14 20:25:43 +0530

    Tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+
+// npx tailwindcss init
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
