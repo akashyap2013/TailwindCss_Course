7dcb26b779b10d769cfe034003763fd54fe21822
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2021-07-15 20:26:33 +0530

    JIT Compiler

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
-
-// npx tailwindcss init
-
 module.exports = {
-  purge: [],
+  mode: 'jit',
+  purge: [
+    './public/**/*.{html,js}',
+    './public/index.html'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
