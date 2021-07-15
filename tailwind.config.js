a1b3eb5684fc88d1bd8638e9d29a796ae67c49a1
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2021-07-15 20:58:39 +0530

    Theme Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 module.exports = {
-  mode: 'jit',
-  purge: [
-    './public/**/*.{html,js}',
-    './public/index.html'
-  ],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens : {
+      sm: '400px',
+      md: "700px",
+      lg: '900px',
+      xl: '1400px'
+    },
+    colors: {
+      indigo : {
+        light: '#b3bcf5',
+        DEFAULT: '#5c6ac4',
+        dark: '#202e78',
+      }
+    },
+    spacing : {
+      1 : '0.35rem',
+      2: '0.75rem'
+    },
     extend: {},
   },
   variants: {
