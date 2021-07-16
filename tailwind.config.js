ecd62a8f2066e83ebec6e9f8dbcbb8c5c0a8c13d
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2021-07-16 20:55:36 +0530

    Extends Variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,13 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      screens : {
-        '3xl' : '1600px'
-      },
-      colors: {
-        'regal-blue' : '#243c5a'
-      },
-      spacing :{
-        '13' : '3.25rem',
-        '15' : '3.75rem',
-        '128': '32rem',
-        '144': '36rem',
-      }
-    },
+    extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor : ['active'],
+      textColor : ['visited']
+    },
   },
   plugins: [],
 }
