9fc23b61997e3f618ebcbff87162020564b51979
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2021-07-18 18:39:58 +0530

    Plugin AddUtilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+
+const plugin  = require('tailwindcss/plugin')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
@@ -6,9 +9,21 @@ module.exports = {
   },
   variants: {
     extend: {
-      backgroundColor : ['active'],
-      textColor : ['visited']
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ( { addUtilities }) {
+      const newUtilities = {
+        '.scale-1' : {
+          transform : 'scale(1)'
+        },
+        '.rotate-1' : {
+          transform : 'rotate(1)'
+        }
+      }
+        addUtilities(newUtilities, {
+          variants : ['responsive', 'hover']
+        })
+    })
+  ],
 }
