f9f38c85455c68d810b5b1ce70a156d39c93e24b
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2021-07-18 19:33:01 +0530

    Preset

---
+++ preset/tailwind.preset.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+      extend: {
+        padding : {
+          '4.5' : '4.5rem'
+        }
+      },
+    }
+  }
+  
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,17 @@
-
-const plugin  = require('tailwindcss/plugin')
-
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+
+    },
   },
   variants: {
     extend: {
     },
   },
-  plugins: [
-    plugin(function ( { addBase, theme }) {
-      const heading = {
-        'h1' : { fontSize : theme('spacing.6')},
-        'h2' : { fontSize : theme('fontSize.xl') },
-        'h3' : { fontSize : theme('fontSize.lg') },
-      }
-
-      addBase(heading)
-    })
-  ],
+  plugins: [],
+  presets : [
+    require('./preset/tailwind.preset')
+  ]
 }
